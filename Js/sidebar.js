ee33cdb5a867f09710bc6c25e44ebf738e083640
ValeFeito <91625939+ValeFeito@users.noreply.github.com>
2021-11-07 18:07:52 -0300

    Add files via upload

--- Js/sidebar.js
+++ Js/sidebar.js
@@ -35,3 +35,8 @@ function validarTel() {
     }
 
   
+    var myCarousel = document.querySelector('#myCarousel')
+    var carousel = new bootstrap.Carousel(myCarousel, {
+      interval: 2000,
+      wrap: false
+    })
